f33609029749db7b8235849aed668885557b9deb
logzh <zjhcug@qq.com>
2016-11-19 16:46:42 +0800

    update

--- resource/component/Hello/index.jsx
+++ resource/component/Hello/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 var isNode = typeof window === 'undefined';
-if (!isNode){
+if (!isNode) {
   require('./index.css');
+}
+
+class Greeting extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
 }
 
 var Hello = React.createClass({
@@ -16,7 +22,9 @@ var Hello = React.createClass({
   },
   render: function() {
 
-    return <p className="hello" onClick={this.handleClick}>hello {this.state.name}, 你点击我的次数:{this.state.count}</p>;
+    return <p className="hello" onClick={this.handleClick}>
+      <Greeting name={this.state.name}/>, 你点击我的次数:{this.state.count}
+    </p>;
   }
 });
 
