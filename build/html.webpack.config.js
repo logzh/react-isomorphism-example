d60aff882fa43417f68ddf4eb1c02d9d45d0f556
logzh <zjhcug@qq.com>
2017-03-29 04:49:40 +0800

    yarn webpack2.0

---
+++ build/html.webpack.config.js
@@ -0,0 +1,45 @@
+var srcDir = './resource/html';
+var distDir = '../views/';
+var config = [
+  {
+    title: '首页',
+    filename: distDir + 'home.html',
+    template: srcDir + '/home.html',
+    chunks: []
+  },{
+    title: 'ejs',
+    filename: distDir + 'ejs.html',
+    template: srcDir + '/ejs.html',
+    chunks: ['vendor', 'ejs-demo']
+  },{
+    title: 'hello',
+    filename: distDir + 'hello.html',
+    template: srcDir + '/hello.html',
+    chunks: ['vendor', 'hello']
+  },
+  {
+    title: 'cart',
+    filename: distDir + 'cart.html',
+    template: srcDir + '/cart.html',
+    chunks: ['vendor', 'cart']
+  },
+  {
+    title: 'cart-iso',
+    filename: distDir + 'cart-iso.html',
+    template: srcDir + '/cart.html',
+    chunks: ['vendor', 'cart-iso']
+  },
+  {
+    title: 'async',
+    filename: distDir + 'async.html',
+    template: srcDir + '/async.html',
+    chunks: ['vendor', 'async']
+  },{
+    title: 'hello-tig',
+    filename: distDir + 'hello-tig.html',
+    template: srcDir + '/hello-tig.html',
+    chunks: ['vendor', 'hello-tig']
+  }
+];
+
+module.exports = config;
